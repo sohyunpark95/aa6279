4ed370f1ec0aa1ad79bf6144d151edcb79e78f90
sohyunpark95 <57379451+sohyunpark95@users.noreply.github.com>
2019-11-18 01:08:31 +0900

    Ghost_image

--- sketch.js
+++ sketch.js
@@ -1,6 +1,12 @@
 let canvas;
 let canvasWidth = 600;
 let canvasHeight = 400;
+var Ghost;
+
+function preload() {
+  Ghost = loadAnimation('Ghost/ghost_1.png', 'Ghost/ghost_3.png');
+  
+}
 
 function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
@@ -9,6 +15,6 @@ function setup() {
 }
 
 function draw() {
-  background(30);
-  rect(mouseX, mouseY, 100, 100);
+  background(255);
+  animation(Ghost, 250, 200);
 }
