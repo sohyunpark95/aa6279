bc3a1247e6f4ea44f1a97ce4367e5ee3ccefdbeb
sohyunpark95 <57379451+sohyunpark95@users.noreply.github.com>
2019-11-18 01:02:31 +0900

    normal_Project

---
+++ sketch.js
@@ -0,0 +1,14 @@
+let canvas;
+let canvasWidth = 600;
+let canvasHeight = 400;
+
+function setup() {
+  canvas = createCanvas(canvasWidth, canvasHeight);
+  canvas.position(windowWidth/2 - canvasWidth/2, 20);
+  noCursor();
+}
+
+function draw() {
+  background(30);
+  rect(mouseX, mouseY, 100, 100);
+}
