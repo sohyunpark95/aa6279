48c44a71e7af7a59a4f6536d2852705db741285e
sohyunpark95 <57379451+sohyunpark95@users.noreply.github.com>
2019-12-06 22:37:27 +0900

    movesquid_down

--- sketch.js
+++ sketch.js
@@ -2,33 +2,36 @@ let canvas;
 let canvasWidth = 600;
 let canvasHeight = 400;
 let squid;
+let x, y;
+
 
 function setup() {
-  background(255,100,100);
+  background(0,0,0);
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
+  x = 280;
+  y = 0;
 //  noCursor();
-  resetGame();
+//  resetGame();
 }
 
 function draw() {
 //  rect(mouseX, mouseY,40,200);
-rect(0,200,140,200);
-rect(320,200,140,200);
+//stroke(50);
+
+rect(0,300,140,100);
+rect(460,300,140,100);
 fill(255,240,180,250);
-rect(460,200,140,200);
-rect(140,200,140,200);
+rect(320,300,140,100);
+rect(140,300,140,100);
+fill(255,255,255);
+rect(x, y, 40, 100);
+x = x ;
+// Moving up at a constant speed
+y = y + 1;
+
+// Reset to the bottom
+if (y >= 300) {
+  y = 300;
 }
-
-function resetGame() {
-  fill(255,255,255);
-squid = createSprite(300,200,40,200);
-
-}
-
-function keyPressed() {
-  if (keyCode == DOWN_ARROW) {
-    squid.position.y += 10;
-  }
-drawSprites();
 }
