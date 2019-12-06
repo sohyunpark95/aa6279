dcc3f0c8ca3c518d3d47a9f4650f2f58294a1715
sohyunpark95 <57379451+sohyunpark95@users.noreply.github.com>
2019-12-06 22:00:15 +0900

    move_squid_1

--- sketch.js
+++ sketch.js
@@ -1,23 +1,34 @@
 let canvas;
 let canvasWidth = 600;
 let canvasHeight = 400;
-let squid
+let squid;
 
 function setup() {
+  background(255,100,100);
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
-  noCursor();
-//  resetGame();
+//  noCursor();
+  resetGame();
 }
 
 function draw() {
-  background(30);
-  rect(mouseX, mouseY,40,200);
+//  rect(mouseX, mouseY,40,200);
 rect(0,200,140,200);
 rect(320,200,140,200);
 fill(255,240,180,250);
 rect(460,200,140,200);
-rect(140,200,140,200)
+rect(140,200,140,200);
+}
 
+function resetGame() {
+  fill(255,255,255);
+squid = createSprite(300,200,40,200);
 
+}
+
+function keyPressed() {
+  if (keyCode == DOWN_ARROW) {
+    squid.position.y += 10;
+  }
+drawSprites();
 }
