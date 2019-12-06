6ad680c4d6705505ed1720802c68807d01b6f5da
sohyunpark95 <57379451+sohyunpark95@users.noreply.github.com>
2019-12-06 20:12:18 +0900

    creat_underbox

--- sketch.js
+++ sketch.js
@@ -11,4 +11,8 @@ function setup() {
 function draw() {
   background(30);
   rect(mouseX, mouseY, 100, 100);
+rect(0,200,250,100);
+rect(350,200,250,100);
+rect(0,300,300,100);
+rect(350,300,300,100);
 }
