0a6df3ea6b1b326b7fc9b6d0a08c2b6e66daaced
sohyunpark95 <57379451+sohyunpark95@users.noreply.github.com>
2019-12-06 21:11:26 +0900

    creat_teath

--- sketch.js
+++ sketch.js
@@ -1,18 +1,23 @@
 let canvas;
 let canvasWidth = 600;
 let canvasHeight = 400;
+let squid
 
 function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
   noCursor();
+//  resetGame();
 }
 
 function draw() {
   background(30);
-  rect(mouseX, mouseY, 100, 200);
-rect(0,200,250,100);
-rect(350,200,250,100);
-rect(0,300,250,100);
-rect(350,300,300,100);
+  rect(mouseX, mouseY,40,200);
+rect(0,200,140,200);
+rect(320,200,140,200);
+fill(255,240,180,250);
+rect(460,200,140,200);
+rect(140,200,140,200)
+
+
 }
